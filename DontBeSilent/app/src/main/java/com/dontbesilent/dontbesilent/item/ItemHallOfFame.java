58579c4a0bbdee4da5af394d3cfe213ca3b27bd0
lamhx393 <lamhx393@gmail.com>
2016-10-30 00:06:00 +0700

    update - icon in right fame's name

--- DontBeSilent/app/src/main/java/com/dontbesilent/dontbesilent/item/ItemHallOfFame.java
+++ DontBeSilent/app/src/main/java/com/dontbesilent/dontbesilent/item/ItemHallOfFame.java
@@ -7,5 +7,5 @@ public class ItemHallOfFame {
     public String mAvata="https://www.enrole.com/ipfw/images/custom/course/0025_CMO_IconLB_Supply-Chain_RGB.png";
     public String mName="Hoang Xuan Lam";
     public String mProj ="100 Proj";
-    public boolean isPersuation = false;
+    public boolean isPersuation = true;
 }
