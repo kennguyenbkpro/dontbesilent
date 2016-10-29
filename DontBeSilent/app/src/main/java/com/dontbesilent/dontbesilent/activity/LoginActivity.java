9d228ef3d54b7eb6ce77355639781f9f13d7f5a6
khanhnq2 <khanhnq2@vng.com.vn>
2016-10-29 11:59:48 +0700

    add gradle

---
+++ DontBeSilent/app/src/main/java/com/dontbesilent/dontbesilent/activity/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.dontbesilent.dontbesilent.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by khanhnq2 on 29/10/2016.
+ */
+
+public class LoginActivity extends Activity{
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+            if (mFirebaseUser.getPhotoUrl() != null) {
+                mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+            }
+        }
+    }
+}
